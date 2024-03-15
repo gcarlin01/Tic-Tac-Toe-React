b8a014d32e1f9f5e99d9b6b9557fbea29832d942
gcarlin01 <giancarlo.carlin01@gmail.com>
2024-03-14 21:07:48 -0400

    Updates

---
+++ precache-manifest.bcdce6a2264f0173afecad4663396673.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a344b3c62a7b6f574f88c17f13a2ea",
+    "url": "/Tic-Tac-Toe-React/index.html"
+  },
+  {
+    "revision": "6d27d89e7edf26617a2b",
+    "url": "/Tic-Tac-Toe-React/static/css/main.8c1e7661.chunk.css"
+  },
+  {
+    "revision": "f86f7a9b94aa23be7f29",
+    "url": "/Tic-Tac-Toe-React/static/js/2.91dc03cb.chunk.js"
+  },
+  {
+    "revision": "6d27d89e7edf26617a2b",
+    "url": "/Tic-Tac-Toe-React/static/js/main.6f978833.chunk.js"
+  },
+  {
+    "revision": "096b8a1ecfa5f65a8f91",
+    "url": "/Tic-Tac-Toe-React/static/js/runtime~main.599418ca.js"
+  }
+]);
